a8e13307adec90b68e2ce7d10f5b10a35da8a67c
YuceOzan <yuce.ozan1000@gmail.com>
2024-01-10 20:03:46 +0300

    Initial commit

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+const firstName = "Ozan";
+const age = 28;
+const married = true;
+const height = null;
+const salary = undefined;
+
+console.log(typeof firstName, age, married, height, salary);
+
+
+const value1 = "Hello";
+const value2 = "Goodbye";
+const value3 = 45;
+const value4 = true;
+
+const me = "My name is Ozan ", lastName =  " my last name is Yuce ", whatAge =  " i am " + 28, situation = " and i am married";
+
+const myAge = "I am 28 years old";
+
+const yourAge = "you are 31 years old";
+
+console.log(myAge);
+console.log(yourAge);
---
+++ variable.js
@@ -0,0 +1,14 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavascript
+
+/*
+    comments can make code readable,
+    easy to reuse and informative.
+*/
+
+const name = "Ozan";
+const Birthday = 19/2/1995;
+const unmarried = false;
+const weight = null;
+const income = undefined;
